ab57a4b4d6e709c008b4a696d3fbd8c3a7604505
Josh Aluko <jaluko1000@yahoo.com>
2021-08-18 21:33:26 +0000

    removing Landmark added erroneously. Revert "Landmark comment"
    This reverts commit 5433ffa3b6a7fd950b17e1ecaaa0a49806d30425.

--- facebook/check1.sh
+++ facebook/check1.sh
@@ -2,4 +2,4 @@ echo "enter the filename"
 read filename 
 echo Welcome to Devops $filename
 echo $filename, this is going to be the best thing since slice bread!
-echo Welcome to Landmark Technologies!!!
+
